5e4cd16a1b94c6977afb6fef7d47d8463dacd56b
AxelMedrano <axel.medrano@cua.uam.mx>
2024-04-15 17:09:47 -0600

    4
    Prototipo

--- Carga/carga.c
+++ Carga/carga.c
@@ -16,22 +16,53 @@ void seleccionar_carga() {
     estado_carga = (estado_carga + 1) % 3;
     switch(estado_carga) {
         case LIGERA:
-        // Pendiente
+        encender_led1();
+        apagar_led2();
         case MEDIA:
-        // Pendiente
+        apagar_led1();
+        encender_led1();
         case PESADA:
-        // Pendiente
+        encender_led1();
+        encender_led2();
     }
 }
 
-void encender_led() {
-    //Pendiente
+void encender_led1() {
+    gpio_put(LED_1_PIN, 1);
 }
 
+void encender_led2() {
+    gpio_put(LED_2_PIN, 1)
+}
+
+void apagar_led1() {
+    gpio_put(LED_1_PIN, 0)
+}
+
+void apagar_led2() {
+    gpio_put(LED_2_PIN, 0)
+}
+
+
 bool verificar_boton_presionado() {
-    //Pendiente
+    return !gpio_get(BOTON_PIN);
 }
 
 int main() {
-    //Pendiente
+    gpio_init(LED_1_PIN);
+    gpio_init(LED_2_PIN);
+    gpio_init(BOTON_PIN);
+    gpio_set_dir(LED_1_PIN, GPIO_OUT);
+    gpio_set_dir(LED_2_PIN, GPIO_OUT);
+    gpio_set_dir(BOTON_PIN, GPIO_IN);
+    
+    while(true) {
+        if(verificar_boton_presionado()) {
+            sleep_ms(1500); 
+            if(verificar_boton_presionado()) {
+                seleccionar_carga();
+            }
+        }
+    }
+    return 0;
 }
