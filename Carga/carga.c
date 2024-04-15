1d9d81d451fd678e33e7c8d81d6e9c82cfd4e30a
AxelMedrano <axel.medrano@cua.uam.mx>
2024-04-15 16:20:54 -0600

    3
    Elimine centrifugar y empecé a construir el esqueleto de "carga"

--- Carga/carga.c
+++ Carga/carga.c
@@ -1,4 +1,37 @@
-#include <stdio.h>
+#include <stdbool.h>
 #include "pico/stdlib.h"
-#include "hardware/gpio.h" 
 
+#define LIGERA 0
+#define MEDIA 1
+#define PESADA 2
+
+#define LED_1_PIN 10
+#define LED_2_PIN 11
+#define BOTON_PIN 15
+
+
+int estado_carga = LIGERA
+
+void seleccionar_carga() {
+    estado_carga = (estado_carga + 1) % 3;
+    switch(estado_carga) {
+        case LIGERA:
+        // Pendiente
+        case MEDIA:
+        // Pendiente
+        case PESADA:
+        // Pendiente
+    }
+}
+
+void encender_led() {
+    //Pendiente
+}
+
+bool verificar_boton_presionado() {
+    //Pendiente
+}
+
+int main() {
+    //Pendiente
+}
