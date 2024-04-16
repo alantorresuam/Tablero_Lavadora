04f4b4aa6b00f20b2a2f2679e26ff116a4c42935
Alan torres <alan.torres@cua.uam.mx>
2024-04-15 19:21:44 -0600

    primer entrega

---
+++ Centrifugar/Centrifugar.c
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+#include "pico/stdlib.h"
+
+#define LED_PIN  10 
+#define CENTRIFUGADO_PIN 14 // Botón para activar el centrifugado
+
+void centrifugar();
+
+int main() {
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init(CENTRIFUGADO_PIN);
+    gpio_set_dir(CENTRIFUGADO_PIN, GPIO_IN);
+    gpio_pull_up(CENTRIFUGADO_PIN);
+
+    while (true) {
+        if (gpio_get(CENTRIFUGADO_PIN)) {
+            centrifugar();
+        }
+
+        sleep_ms(100);
+    }
+
+    return 0;
+}
+
+void centrifugar() {
+    gpio_put(LED_PIN, 1); // Encender el LED para indicar que está en centrifugado
+    sleep_ms(1800000); // Esperar 30 minutos (1800000 milisegundos)
+    gpio_put(LED_PIN, 0); // Apagar el LED al finalizar el centrifugado
+    printf("Centrifugado completo.\n");
+}
---
+++ Temoperatura/Temperatura.c
@@ -0,0 +1,48 @@
+#include "botones_temperatura.h"
+#include "pico/stdlib.h"
+
+#define BOTON_PIN  14 
+#define LED_FRIO_PIN 10
+#define LED_TIBIO_PIN 11
+#define LED_CALIENTE_PIN 12
+
+int contador = 0;
+
+void inicializar_botones_temperatura() {
+    gpio_init(BOTON_PIN);
+    gpio_set_dir(BOTON_PIN, GPIO_IN);
+    gpio_pull_up(BOTON_PIN);
+
+    gpio_init(LED_FRIO_PIN);
+    gpio_set_dir(LED_FRIO_PIN, GPIO_OUT);
+
+    gpio_init(LED_TIBIO_PIN);
+    gpio_set_dir(LED_TIBIO_PIN, GPIO_OUT);
+
+    gpio_init(LED_CALIENTE_PIN);
+    gpio_set_dir(LED_CALIENTE_PIN, GPIO_OUT);
+}
+
+void verificar_botones_temperatura() {
+    if (!gpio_get(BOTON_PIN)) {
+        sleep_ms(20); // Debounce
+        if (!gpio_get(BOTON_PIN)) {
+            contador++;
+            if (contador == 1) {
+                gpio_put(LED_FRIO_PIN, 1);
+            } else if (contador == 2) {
+                gpio_put(LED_TIBIO_PIN, 1);
+            } else if (contador == 3) {
+                gpio_put(LED_CALIENTE_PIN, 1);
+            } else if (contador == 4) {
+                gpio_put(LED_FRIO_PIN, 0);
+                gpio_put(LED_TIBIO_PIN, 0);
+                gpio_put(LED_CALIENTE_PIN, 0);
+                contador = 0;
+            }
+            while (!gpio_get(BOTON_PIN)) {
+                sleep_ms(20); // Espera hasta que se suelte el botón
+            }
+        }
+    }
+}
