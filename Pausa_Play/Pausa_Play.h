85d224b56e45530b109c70be7d38bf699dcbd169
Bryan8a <bryan.ochoa@cua.uam.mx>
2024-04-16 21:06:28 -0600

    Pausa_Play
    Se quitó funcionalidad.

--- Pausa_Play/Pausa_Play.h
+++
@@ -1,4 +0,0 @@
-// Esta función pausa la lavadora e indica que la lavadora está pausada
-bool pausa_play();
-
-bool play();
--- Pausa_Play/Pausa_play.c
+++ Pausa_Play/Pausa_play.c
@@ -1,28 +1,6 @@
-#include <stdbool.h>
-#include "Pausa_Play.h"
-#include "pico/stdlib.h"
+#include <stdio.h>
 
-# define LED_PIN  8
-# define BUTTON_PIN 9
-
-bool pausa_Play(bool pausado){
-    if(!gpio_get(BUTTON_PIN)){
-        sleep_ms(1500);
-        if (!gpio_get(BUTTON_PIN) && pausado){
-            play();
-            return false;
-        } else if(!pausado){
-            pausa();
-            return true;
-        }
-    }
-    return pausado;
-}
-
-void pausa(){
-    gpio_put(LED_PIN, 1); // Cuando da pausa se enciende el LED
-}
-
-void play(){
-    gpio_put(LED_PIN, 0); // Cuando da play se apaga el LED
+int main() {
+    printf("Hola Mundo\n");
+    return 0;
 }
