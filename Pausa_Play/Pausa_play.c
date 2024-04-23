ad150e29e4d4eed90e6817c5344a534eddb62022
Bryan8a <bryan.ochoa@cua.uam.mx>
2024-04-23 13:13:14 -0600

    Pausa y Play
    Se agrega el modulo para pausa y play

--- Pausa_Play/Pausa_play.c
+++ Pausa_Play/Pausa_play.c
@@ -1,9 +1,28 @@
-#include "pico/stdlib.h"
+#include <stdbool.h>
 #include "Pausa_play.h"
+#include "pico/stdlib.h"
 
+# define LED_PIN17  17
+# define BUTTON_PIN1 1
 
-# define LED_PIN22 22
+bool pausa_play(bool pausado){
+    if(!gpio_get(BUTTON_PIN1)){
+        sleep_ms(1500);
+        if (!gpio_get(BUTTON_PIN1) && pausado){
+            play();
+            return false;
+        } else if(!pausado){
+            pausa();
+            return true;
+        }
+    }
+    return pausado;
+}
 
 void pausa(){
-    gpio_put(LED_PIN22, 1); // Encender el LED
+    gpio_put(LED_PIN17, 1); // Pausar el LED
+}
+
+void play(){
+    gpio_put(LED_PIN17, 0); // Play de el LED
 }
--- Pausa_Play/Pausa_play.h
+++ Pausa_Play/Pausa_play.h
@@ -1,4 +1,15 @@
 /**
- * @brief Turns on the delicate washing mode LED.
+ * @brief This function turns on the washing machine and indicates that the washing machine is on.
+ * @return true if the washing machine is successfully turned on, false otherwise.
+ */
+bool pausa_play(bool encendido);
+
+/**
+ * @brief Turns off the washing machine.
+ */
+void play();
+
+/**
+ * @brief Turns on the washing machine.
  */
 void pausa();
