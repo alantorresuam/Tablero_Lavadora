58c8564f2a8edb2f553391f65223afa7a332e6f8
AxelMedrano <axel.medrano@cua.uam.mx>
2024-04-24 12:06:29 -0600

    Actualización 3 leds
    Se me ha asignado un nuevo led, por lo tanto la carga ya utiliza 3 leds.

--- carga_ropa/carga.c
+++ carga_ropa/carga.c
@@ -4,18 +4,16 @@
 
 # define LED_PIN10 10
 # define LED_PIN11 11
+# define LED_PIN12 12
 
 void liviana(){
-    gpio_put(LED_PIN11, 0); // Encender el LED
     gpio_put(LED_PIN10, 1); // Encender el LED
 }
 
 void media(){
-    gpio_put(LED_PIN10, 0); // Encender el LED
     gpio_put(LED_PIN11, 1); // Encender el LED
 }
 
 void pesada(){
-    gpio_put(LED_PIN10, 1); // Encender el LED
-    gpio_put(LED_PIN11, 1); // Encender el LED    
+    gpio_put(LED_PIN12, 1); // Encender el LED 
 }
