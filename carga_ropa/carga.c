e58b6cf407712473fabbcd60abe3d2578e0299e8
AxelMedrano <axel.medrano@cua.uam.mx>
2024-04-24 13:34:58 -0600

    Apaga los dos leds no ocupados
    Apaga los leds no ocupados para que solo quede prendido el led que corresponde.

--- carga_ropa/carga.c
+++ carga_ropa/carga.c
@@ -7,13 +7,19 @@
 # define LED_PIN12 12
 
 void liviana(){
+    gpio_put(LED_PIN11, 0); // Apagar el LED
+    gpio_put(LED_PIN12, 0); // Apagar el LED
     gpio_put(LED_PIN10, 1); // Encender el LED
 }
 
 void media(){
+    gpio_put(LED_PIN10, 0); // Apagar el LED
+    gpio_put(LED_PIN12, 0); // Apagar el LED
     gpio_put(LED_PIN11, 1); // Encender el LED
 }
 
 void pesada(){
+    gpio_put(LED_PIN10, 0); // Apagar el LED
+    gpio_put(LED_PIN11, 0); // Apagar el LED
     gpio_put(LED_PIN12, 1); // Encender el LED 
 }
---
+++ display_lavadora/display.c
@@ -0,0 +1,11 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "display.h"
+
+
+
+
+
+int main() {
+
+}
