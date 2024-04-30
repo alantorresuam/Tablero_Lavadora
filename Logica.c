48f7ef21b048b3c7120586b08dd4908eadbc8bad
Alan torres <alan.torres@cua.uam.mx>
2024-04-30 10:43:50 -0600

    correcciones ya aplicadas y final de la logica

--- Logica.c
+++ Logica.c
@@ -1,5 +1,6 @@
 #include "Teclado.h"
 #include "temporizador.h"
+#include "leds.h"
 #include "pin_list_display.h"
 #include "pin_list.h"
 #include <stdio.h>
@@ -23,11 +24,11 @@ int main()
     int tiempo = 0;
     int segundo = 0;
 
-    bool encendido = false;
-    bool pausa = true;
-    bool lavar = false;
-    bool enjuagar = false;
-    bool centrifugar = false;
+    bool seleccion_encendido = false;
+    bool seleccion_pausa = true;
+    bool seleccion_lavar = false;
+    bool seleccion_enjuagar = false;
+    bool seleccion_centrifugar = false;
     bool enproceso = false;
 
     bool lavado_seleccionado = false;
@@ -50,50 +51,167 @@ int main()
      
     char opcion = '\0';
     while(true){
-        if(encendido){
+        if(seleccion_encendido){
             char opcion = read_keypad();
-            if (pausa){
+            if (seleccion_pausa){
                 if(!enproceso){
                     //leer opciones del ciclo
-                    if(opcion == '7'){ //lavar
-                        lavar = true;
+                    if(opcion == '7'){ //seleccion_lavar
+                        seleccion_lavar = true;
+                        lavar();
                         tiempo_lavar = 30;
                     }
-                    if(opcion == '8'){ //enjuagar
-                        enjuagar = true;
+                    if(opcion == '8'){ //seleccion_enjuagar
+                        seleccion_enjuagar = true;
                         tiempo_enjuagar = 30;
+                        enjuagar();
                     }
-                    if(opcion == '9'){ //centrifugar
-                        centrifugar = true;
+                    if(opcion == '9'){ //seleccion_centrifugar
+                        seleccion_centrifugar = true;
                         tiempo_centrifugar = 30;
+                        centrifugado();
                     }
 
                     //Tipo lavado
                     if(!lavado_seleccionado){
-                        if(lavar){
+
+                        if(seleccion_lavar){
                             if (opcion == '1')
                             {
                                 lavado_seleccionado = true;
                                 strcpy(tipo_lavado, "\nLavado: delicado\t");
+                                delicado();
                             }
                             if (opcion == '2')
                             {
                                 lavado_seleccionado = true;
                                 strcpy(tipo_lavado, "\nLavado: normal\t");
+                                normal();
                             }
                             if (opcion == '3')
                             {
                                 lavado_seleccionado = true;
                                 strcpy(tipo_lavado, "\nLavado: fuerte\t");
+                                fuerte();
                             }
                             
                         }
                     }
 
-                    //Enjuagar y centrifugar 
-                    if(enjuagar){
+                    //Enjuagar y seleccion_centrifugar 
+                    if(seleccion_enjuagar){
                         strcpy(char_enjuagar, "//\tEnjuagar\t");   
                     }
-                    if(centrifugar){
+                    if(seleccion_centrifugar){
                         strcpy(char_centrifugar, "//\tCentrifugar\t");
                     }
+
+                    //Tipo de carga
+                    if(!carga_seleccionada){
+                        if (opcion == '4')
+                        {
+                            strcpy(tipo_carga, "\nCarga: ligera\t");
+                            carga_seleccionada = true;
+                            liviana();
+                        }
+                        if (opcion == '5')
+                        {
+                            strcpy(tipo_carga, "\nCarga: mediana\t");
+                            carga_seleccionada = true;
+                            media();
+                        }
+                        if (opcion == '6')
+                        {
+                            strcpy(tipo_carga, "\nCarga: pesada\t");
+                            carga_seleccionada = true;
+                            pesada();
+                        }
+
+                    }
+                    
+                    //Temperatura
+                    if(!temperatura_seleccionada){
+
+                        if(seleccion_lavar){
+                            if (opcion == 'A')
+                            {
+                                strcpy(temperatura, "\nAgua fria\t");
+                                temperatura_seleccionada = true;
+                                fria();
+                            }
+                            if (opcion == 'B')
+                            {
+                                strcpy(temperatura, "\nAgua tibia\t");
+                                temperatura_seleccionada = true;
+                                tibia();
+                            }
+                            if (opcion == 'C')
+                            {
+                                strcpy(temperatura, "\nAgua caliente\t");
+                                temperatura_seleccionada = true;
+                                caliente();
+                            }
+                        }
+                    }
+
+                    //Presionar play
+                    if(seleccion_lavar || seleccion_enjuagar || seleccion_centrifugar){
+                        if(lavado_seleccionado && carga_seleccionada){
+                            if(opcion == '*'){
+                                seleccion_pausa = false;
+                                enproceso = true;
+                                sleep_ms(1000);
+                                tiempo = tiempo + tiempo_lavar + tiempo_enjuagar + tiempo_centrifugar;
+                            }
+                        }
+                    }  
+                }else{
+                    if(opcion == '*'){
+                        seleccion_pausa = false;
+                    }
+                }
+                int decena = tiempo / 10;
+                int unidad = tiempo % 10;
+                actualizar(decena, unidad);
+                    
+            }else{ //Play
+                int decena = tiempo / 10;
+                int unidad = tiempo % 10;
+                actualizar(decena, unidad);
+                segundo += 20;
+                if (segundo == 1000){
+                    tiempo = decrementar();
+                    segundo = 0;
+                }
+                if(tiempo == 0){
+                    printf("Ciclo de lavado terminado :) \n Apagando...\n");
+                    sleep_ms(2000);
+                    printf("Apagado\n");
+                    seleccion_encendido = false;
+                    break;
+                }
+                if(opcion == '0'){
+                    seleccion_pausa = true;
+                }
+            }
+            
+            if(opcion == '#'){
+                seleccion_encendido = false;
+                printf("Apagando...\n");
+                apagar();
+                sleep_ms(2000);
+                printf("Apagado\n");
+            }
+        }else{
+            char opcion = read_keypad();
+            if(opcion == 'D'){
+                seleccion_encendido = true;
+                printf("Encendido \n");
+                encender();
+                sleep_ms(500);
+            }
+        }
+
+    }
+    return 0;
+}
