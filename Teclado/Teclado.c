7488de08a290d71ad556c94617eecce9dcabcbbb
Gustavo Angel Morales Vigi <gustavo.231004@gmail.com>
2024-04-30 09:54:22 -0600

    Implementacion de keypad_construct()

--- Teclado/Teclado.c
+++ Teclado/Teclado.c
@@ -1,49 +1,26 @@
 #include "pico/stdlib.h"
-#include <stdio.h>
 #include "hardware/gpio.h"
-
-#define ROWS 4
-#define COLS 4
-
-const uint ROW_PINS[ROWS] = {2, 3, 4, 5}; 
-const uint COL_PINS[COLS] = {6, 7, 8, 9}; 
-
-const char KEYS[ROWS][COLS] = {
-    {'1', '2', '3', 'A'},
-    {'4', '5', '6', 'B'},
-    {'7', '8', '9', 'C'},
-    {'*', '0', '#', 'D'}
-};
+#include "Teclado.h"
+#include "pico/stdlib.h"
 
 
-void keypad_init() {
-    for (int i = 0; i < ROWS; i++) {
-        gpio_init(ROW_PINS[i]);
-        gpio_set_dir(ROW_PINS[i], GPIO_OUT);
-    }
+void keypad_construct(const int a[]){
+    for(int i=0; i<ROWS; i++)
+        ROWS_PINS[i] = a[i];
+    
+    for(int i=0; i<COLS; i++)
+        COLS_PINS[i] = a[i+ROWS];
+    
+    char keyValues[4][4] = {
+        {'1', '2', '3', 'A'},
+        {'4', '5', '6', 'B'},
+        {'7', '8', '9', 'C'},
+        {'*', '0', '#', 'D'}
+    };
 
-    for (int i = 0; i < COLS; i++) {
-        gpio_init(COL_PINS[i]);
-        gpio_set_dir(COL_PINS[i], GPIO_IN);
-        gpio_pull_down(COL_PINS[i]);
-    }
-}
-
-char read_keypad() {
-    // Escanear las filas
-    for (int i = 0; i < ROWS; i++) {
-        gpio_put(ROW_PINS[i], 1);
-
-        // Leer las columnas
-        for (int j = 0; j < COLS; j++) {
-            if (gpio_get(COL_PINS[j]) == 1) {
-                gpio_put(ROW_PINS[i], 0); 
-                return KEYS[i][j]; 
-            }
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            KEYS[i][j] = keyValues[i][j];
         }
-
-        gpio_put(ROW_PINS[i], 0); 
     }
-
-    return '\0'; 
 }
