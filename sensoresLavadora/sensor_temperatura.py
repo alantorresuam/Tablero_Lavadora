bfdbdac8fc3c2f52a830b07a39f328fa3cffcfc9
AxelMedrano <axel.medrano@cua.uam.mx>
2024-05-27 15:02:05 -0600

    esqueleto temperatura
    definir métodos del sensor de temperatura.

---
+++ sensoresLavadora/sensor_temperatura.py
@@ -0,0 +1,17 @@
+import machine
+import onewire
+import ds18x20
+import time
+
+def init_temperatura(pin_num: int):
+
+    return ??
+
+def obtener_temperaturas(ds_sensor, roms):
+
+    return ??
+
+
+def nivel_temperatura(temp):
+
+    return ??
