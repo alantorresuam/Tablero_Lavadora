31e9b354deb69573f4bebed3020a7aa3ae2f33ab
AxelMedrano <axel.medrano@cua.uam.mx>
2024-05-27 15:04:22 -0600

    Inicializar temperatura
    Programé la lógica necesaria para inicializar el sensor de temperatura.

--- sensoresLavadora/sensor_temperatura.py
+++ sensoresLavadora/sensor_temperatura.py
@@ -4,8 +4,10 @@ import ds18x20
 import time
 
 def init_temperatura(pin_num: int):
-
-    return ??
+    pin = machine.Pin(pin_num)
+    ds_sensor = ds18x20.DS18X20(onewire.OneWire(pin))
+    roms = ds_sensor.scan()
+    return ds_sensor, roms
 
 def obtener_temperaturas(ds_sensor, roms):
 
