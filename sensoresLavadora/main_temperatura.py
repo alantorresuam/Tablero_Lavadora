f41c8baa29cdd1565f0f3eb5fe339c534e5c6c9e
AxelMedrano <axel.medrano@cua.uam.mx>
2024-05-27 15:08:53 -0600

    Archivo pruebas
    Agregué archivo main para poder probar el sensor de temperatura.

---
+++ sensoresLavadora/main_temperatura.py
@@ -0,0 +1,19 @@
+from sensor_temperatura import init_temperatura, obtener_temperaturas, nivel_temperatura
+from utime import sleep
+
+def main():
+    pin_num = 16  # Reemplaza '16' con el pin correcto
+    ds_sensor, roms = init_temperatura(pin_num)
+
+    while True:
+        temperaturas = obtener_temperaturas(ds_sensor, roms)
+        
+        for temp in temperaturas:
+            temp_celsius = f"{temp:.2f}°C"
+            nivel = nivel_temperatura(temp)
+            print(f"Temperatura: {temp_celsius}, Nivel: {nivel}")
+        
+        sleep(1)
+
+if __name__ == "__main__":
+    main()
