37514be5e308b880ea873865c5814602bb24ffa6
AxelMedrano <axel.medrano@cua.uam.mx>
2024-05-27 15:14:32 -0600

    Nivel de agua 10 bits
    Modifiqué la lectura de nivel de agua para que se pase la medida de 16 a 10 bits.

--- sensoresLavadora/sensor_nivel_agua.py
+++ sensoresLavadora/sensor_nivel_agua.py
@@ -5,8 +5,9 @@ def init_nivel_agua():
     return adc
 
 def leer_nivel_agua(adc):
-    value_16bit = adc.read_u16()   
-    return value_16bit
+    value_16bit = adc.read_u16()  
+    value_10bit = value_16bit >> 6  
+    return value_10bit
 
 def interpretar_nivel_agua(value):
     return #
