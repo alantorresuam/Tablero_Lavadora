57f99ff5240e2c3e5aea25a0ae0a4e0d03d6adce
Gustavo Angel Morales Vigi <gustavo.231004@gmail.com>
2024-05-28 11:46:58 -0600

    agregar shared_object.py

---
+++ Python/shared_obj.py
@@ -0,0 +1,20 @@
+from fsm import FSM
+
+ev = {
+    'encender' : 0, 
+    'apagar' : 1, 
+    'enjuagar' : 2, 
+    'centrifugar' : 3, 
+    'lavado fin' : 4,
+    'lavar y enjuagar' : 5,
+    'lavar y centrifugar' : 6,
+    'pausar' : 7,       
+    'enjuagado fin' : 8,    
+    'centrifugado fin' : 9, 
+    'lavando' : 10,     
+    'enjuagando': 11,
+    'centrifugando': 12,
+    'lavar': 13
+    }
+
+fsm: FSM = FSM()
