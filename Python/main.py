e2eacf66bdaf32febb8b9bd51b50770e37c9d99e
Gustavo Angel Morales Vigi <gustavo.231004@gmail.com>
2024-05-28 16:06:15 -0600

    implementacion fsm

--- Python/main.py
+++ Python/main.py
@@ -10,10 +10,6 @@ from shared_obj import ev, fsm
 # Configuración de pines UART
 uart = machine.UART(0, baudrate=9600, tx=machine.Pin(12), rx=machine.Pin(13))
 fsm_actions.init_fsm(fsm, ev)
-i2c = I2C(0, sda=Pin(18), scl=Pin(19), freq=400000)
-oled = SSD1306_I2C(128,64,i2c)
-oled.fill(0)
-oled.show()
 
 state = 0
 
@@ -45,25 +41,9 @@ if __name__ == '__main__':
                 if(opcion_teclado == 'D'): # encender
                     fsm.compute_next_state(shared_obj.ev['encender'])  
                     state = fsm.get_current_state()
-                    inicio = 0;     
-                    while inicio < 3:
-                        oled.text("bienvenido", 30, 20)
-                        oled.show()
-                        sleep(1)
-                        inicio += 1
-                    oled.fill(0)
-                    oled.show()
+                    
         elif state == 1:
-            oled.fill(0)
-            oled.text(f"Selecciona", 20, 0)
-            oled.text(f"una opcion", 20, 8)
-            if(lavar):
-                oled.text(f"Lavar", 20, 20)
-            if(enjuagar):
-                oled.text(f"Enjuagar", 20, 30)
-            if(centrifugar):
-                oled.text(f"Centrifugar", 20, 40)
-            oled.show()                            
+                            
             if comando == 'K':
                 while uart.any() == 0:
                     continue
@@ -126,12 +106,6 @@ if __name__ == '__main__':
             if(Iniciar_temporizador):
                 uart.write(b'I') # Iniciar temporizador
                 Iniciar_temporizador = False
-            oled.fill(0)
-            oled.text(f"Lavando...", 20, 8)
-            oled.text(f"{lavado}", 20, 20)
-            oled.text(f"{carga}", 20, 30)
-            oled.text(f"{temperatura}", 20, 40)
-            oled.show()
             if comando == 'T':
                 if not centrifugar and not enjuagar:
                     fsm.compute_next_state(ev['lavado fin'])
@@ -140,10 +114,6 @@ if __name__ == '__main__':
                     lavar = False
                     enjuagar = False
                     centrifugar = False
-                    oled.fill(0)
-                    oled.text("Proceso terminado", 0, 8)
-                    oled.show()
-                    sleep(3)
                 elif enjuagar:
                     fsm.compute_next_state(ev['lavar y enjuagar'])
                     state = fsm.get_current_state()
@@ -176,11 +146,7 @@ if __name__ == '__main__':
             if(Iniciar_temporizador):
                 uart.write(b'I') # Iniciar temporizador
                 Iniciar_temporizador = False
-            oled.fill(0)
-            oled.text(f"Enjuagando...", 20, 8)
-            oled.text(f"{carga}", 20, 20)
-            oled.text(f"{temperatura}", 20, 30)
-            oled.show()
+                
             if comando == 'T':
                 if (not centrifugar):
                     fsm.compute_next_state(ev['enjuagado fin'])
@@ -189,10 +155,7 @@ if __name__ == '__main__':
                     lavar = False
                     enjuagar = False
                     centrifugar = False
-                    oled.fill(0)
-                    oled.text(f"Proceso terminado", 20, 8)
-                    oled.show()   
-                    sleep(3)               
+             
                 else:
                     fsm.compute_next_state(ev['centrifugar'])
                     state = fsm.get_current_state()
@@ -219,10 +182,6 @@ if __name__ == '__main__':
             if(Iniciar_temporizador):
                 uart.write(b'I') # Iniciar temporizador
                 Iniciar_temporizador = False
-            oled.fill(0)
-            oled.text(f"Centrifugando...", 8, 8)
-            oled.text(f"{carga}", 20, 20)
-            oled.show()
             if comando == 'T':
                 fsm.compute_next_state(ev['centrifugado fin'])
                 lavado, carga, temperatura = lavados[1], cargas[1], temperaturas[1]
@@ -230,10 +189,7 @@ if __name__ == '__main__':
                 lavar = False
                 enjuagar = False
                 centrifugar = False
-                oled.fill(0)
-                oled.text(f"Proceso terminado", 20, 8)
-                oled.show() 
-                sleep(3)                               
+                                              
             if comando == 'K':
                 while uart.any() == 0:
                     continue
@@ -253,10 +209,6 @@ if __name__ == '__main__':
                     
         elif state == 5:
             uart.write(b"P")
-            oled.fill(0)
-            oled.text(f"Pausa...", 20, 8)
-            oled.text(f"Proceso: {procesos[estado_anterior - 2]}", 0, 20)
-            oled.show()
             if comando == 'K':
                 while uart.any() == 0:
                     continue
