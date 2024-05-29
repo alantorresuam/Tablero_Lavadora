f12ff9b819a40aa85c6a309a63e5650a74c9d9ec
AxelMedrano <125591190+AxelMedrano@users.noreply.github.com>
2024-05-29 13:34:47 -0600

    Update main_temperatura.py

--- Python/sensoresLavadora/main_temperatura.py
+++ Python/sensoresLavadora/main_temperatura.py
@@ -1,6 +1,6 @@
 from sensor_temperatura import init_temperatura, obtener_temperaturas
 
 if __name__ == "__main__":
-    sensor, rom = init_temperatura(16)  
+    sensor, rom = init_temperatura(18)  
     temperatura = obtener_temperaturas(sensor, rom)
     print(f"Temperatura: {temperatura[0]:.2f}°C")
